01dc97466295f9ff5770bfadd30ba814bcc0c5a7
Roman Tkachenko <roman@mailgunhq.com>
2014-09-12 18:26:38 -0700

    Introduce TestApp to simplify unit testing.

---
+++ testutils/testapp.go
@@ -0,0 +1,31 @@
+package testutils
+
+import (
+	"net/http/httptest"
+
+	"github.com/gorilla/mux"
+	"github.com/mailgun/scroll"
+)
+
+// TestApp should be used in unit tests.
+type TestApp struct {
+	RestHelper
+	*scroll.App
+	testServer *httptest.Server
+}
+
+func NewTestApp() *TestApp {
+	router := mux.NewRouter()
+
+	config := &scroll.AppConfig{
+		Name:     "testapp",
+		Host:     "0.0.0.0",
+		Port:     5060,
+		Router:   router,
+		Register: false,
+	}
+
+	testServer := httptest.NewServer(router)
+
+	return &TestApp{scroll.NewApp(config), testServer}
+}
